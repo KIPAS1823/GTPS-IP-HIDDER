d1cbb4d058e1cb571651027f3d0304c2863561c7
KIPAS1823 <69453197+KIPAS1823@users.noreply.github.com>
2020-09-06 15:42:16 +0700

    Update gtps_ip_hidder.py

--- gtps_ip_hidder.py
+++ gtps_ip_hidder.py
@@ -8,13 +8,11 @@ def threadings():
 def making():
 	ip = str(random.randint(1,255)) + '.' + str(random.randint(1, 255)) + '.' + str(random.randint(1,255)) + '.' + str(random.randint(1, 255))
 	a = ''
-	rand = random.randint(0, 25)
+	rand = random.randint(1, 25)
 	for i in range(rand):
 		a += '###'
 	f = open('hosts.txt', 'a+')
-	f.write(str( a + ' ' + ip + " growtopia1.com\n" + ip + ' growtopia2.com\n'))
+	f.write(str( a + ' ' + ip + " growtopia1.com\n" + a + ' ' + ip + ' growtopia2.com\n'))
 	f.close()
 	
 threadings()
-
-#(C) Made By KIPAS#1823
